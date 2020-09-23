b37996ab70285eb359b303cc6342303f9a5cb614
Ajay Kumar <sliet584@gmail.com>
2020-09-23 22:04:10 +0530

    initial update

---
+++ forntend/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { BrowserRouter, Link, Route} from 'react-router-dom';
+import data from './data';
+import './App.css';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+
+function App() {
+  const openMenu = ()=>{
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const colseMenu = ()=>{
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+  return (
+  <BrowserRouter>
+    <div className="grid-container">
+        <header className="header">
+            <div className="brand">
+                <button onClick={openMenu}>
+                    &#9776;
+                </button>
+                <Link to="/">amazona</Link>
+            </div>
+            <div className="header-links">
+                <a href="cart.html">Cart</a>
+                <a href="signin.html">Sign In</a>
+            </div>
+        </header>
+        <aside className="sidebar">
+            <h3> Shoping Categories </h3>
+            <button className="sidebar-close-button" onClick={colseMenu}> x </button>
+            <ul>
+                <li>
+                    <a href="index.html">Pants</a>
+                </li>
+                <li>
+                    <a href="index.html">Shirts</a>
+                </li>
+            </ul>
+        </aside>
+        <main className="main">
+            <div className="content">
+              <Route path="/product/:id" component={ProductScreen} />
+              <Route path="/" exact={true} component={HomeScreen} />
+            </div>
+        </main>
+        <footer className="footer">
+            All right reserved.
+        </footer>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ forntend/src/data.js
@@ -0,0 +1,64 @@
+export default  {
+    products:[
+        {
+        _id : '1',
+        name : 'Full Shirt',
+        categories : 'Shirts',
+        image : '/images/pro1.jpg',
+        price : 600,
+        brand : 'Nike',
+        rating : 3.5,
+        numRivews : 10
+    },
+    {
+        _id : '2',
+        name : 'Fit Shirt',
+        categories : 'Shirts',
+        image : '/images/pro1.jpg',
+        price : 300,
+        brand : 'Puma',
+        rating : 4.5,
+        numRivews : 10
+    },
+    {
+        _id : '3',
+        name : 'Slim Shirt',
+        categories : 'Shirts',
+        image : '/images/pro1.jpg',
+        price : 600,
+        brand : 'Rebok',
+        rating : 4.0,
+        numRivews : 10
+    },
+    {
+        _id : '4',
+        name : 'Slim Shirt',
+        categories : 'Shirts',
+        image : '/images/pro1.jpg',
+        price : 600,
+        brand : 'Rebok',
+        rating : 4.0,
+        numRivews : 10
+    },
+    {
+        _id : '5',
+        name : 'Slim Shirt',
+        categories : 'Shirts',
+        image : '/images/pro1.jpg',
+        price : 600,
+        brand : 'Rebok',
+        rating : 4.0,
+        numRivews : 10
+    },
+    {
+        _id : '6',
+        name : 'Slim Shirt',
+        categories : 'Shirts',
+        image : '/images/pro1.jpg',
+        price : 600,
+        brand : 'Rebok',
+        rating : 4.0,
+        numRivews : 10
+    },
+]
+}
